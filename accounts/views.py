524a33b69c2066d87e667541ef1ad782cb6ee84d
sonmezmusa <sonmezmusa@outlook.com>
2020-12-19 15:38:55 +0300

    views.py değiştirildi
    'views.py'de modelden çekilen veriler 'context'
    ile template'lere gönderildi.

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render, HttpResponse
+from .models import *
 
 # Create your views here.
 
 # anasayfa
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'orders' : orders,
+        'customers' : customers,
+        'total_orders' : total_orders,
+        'delivered' : delivered,
+        'pending' : pending
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 # ürünler
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    context = {
+        'products' : products
+    }
+    return render(request, 'accounts/products.html', context)
 
 
 # müşteri
