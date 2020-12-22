ebe72ad508f0f6d83ec2ed8df82a14e56c79361e
sonmezmusa <sonmezmusa@outlook.com>
2020-12-23 00:43:33 +0300

    accounts/urls.py değiştirildi
    'order' güncellemek için 'path' eklendi.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('products/', products, name='products'),
     path('customer/<str:pk_test>/', customer, name='customer'),
     path('create_order/', createOrder, name="create_order"),
+    path('update_order/<str:pk>/', updateOrder, name='update_order'),
 ]
