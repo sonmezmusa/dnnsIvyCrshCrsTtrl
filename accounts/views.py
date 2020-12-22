f8a76f6944471173961ed284c4ff4a499c9e4e32
sonmezmusa <sonmezmusa@outlook.com>
2020-12-23 01:56:49 +0300

    accounts/views.py değiştirildi
    'order' silmek için 'deleteOrder' fonksiyonu eklendi.

--- accounts/views.py
+++ accounts/views.py
@@ -71,3 +71,14 @@ def updateOrder(request, pk):
         'form' : form
     }
     return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {
+        'item' : order
+    }
+    return render(request, 'accounts/delete.html', context)
