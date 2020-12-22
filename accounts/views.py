7d4b20ad2fd1f46832b0d393acb786f876802ec1
sonmezmusa <sonmezmusa@outlook.com>
2020-12-23 00:45:42 +0300

    accounts/views.py değiştirildi
    'order' güncellemek için view eklendi.

--- accounts/views.py
+++ accounts/views.py
@@ -59,3 +59,15 @@ def createOrder(request):
         'form' : form
     }
     return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(request.POST or None, instance=order)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/order_form.html', context)
