9fee1a98f45ce52627491e3e2513a4091c48fcf0
sonmezmusa <sonmezmusa@outlook.com>
2020-12-23 01:55:16 +0300

    accounts/urls.py değiştirildi
    'order' silmek için 'path' eklendi.

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('customer/<str:pk_test>/', customer, name='customer'),
     path('create_order/', createOrder, name="create_order"),
     path('update_order/<str:pk>/', updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', deleteOrder, name='delete_order'),
 ]
