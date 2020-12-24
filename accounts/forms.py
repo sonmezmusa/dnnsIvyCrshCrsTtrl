a6f4cd5c684cd6884c68c0826abf858d3f8018c7
sonmezmusa <sonmezmusa@outlook.com>
2020-12-24 23:51:11 +0300

    accounts/forms.py değiştirildi
    Register için 'UserCreationForm' miras alınarak
    form oluşturuldu.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
 from .models import *
 
 class OrderForm(forms.ModelForm):
@@ -6,3 +10,14 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2',
+        ]
