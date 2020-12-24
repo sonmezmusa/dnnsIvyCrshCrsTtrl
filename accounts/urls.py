ca9cce3b2763325a3919e231fa0d2a66370c8c59
sonmezmusa <sonmezmusa@outlook.com>
2020-12-24 22:53:02 +0300

    accounts/urls.py değiştirildi
    'register', 'login' için path oluşturuldu.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,13 @@ from .views import *
 app_name = 'accounts'
 
 urlpatterns = [
+    path('register/', registerPage, name='register'),
+    path('login/', loginPage, name='login'),
+
     path('', home, name='home'),
     path('products/', products, name='products'),
     path('customer/<str:pk_test>/', customer, name='customer'),
+    
     path('create_order/<str:pk>/', createOrder, name="create_order"),
     path('update_order/<str:pk>/', updateOrder, name='update_order'),
     path('delete_order/<str:pk>/', deleteOrder, name='delete_order'),
