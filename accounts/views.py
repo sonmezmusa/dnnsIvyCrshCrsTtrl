e0713f54849a592e90e8357ec860903f98b36b16
sonmezmusa <sonmezmusa@outlook.com>
2020-12-25 01:13:02 +0300

    accounts/views.py değiştirildi
    'registerPage' view'de account oluşturuldu mesajı verildi,
    'loginPage' view'de login işlemi yapıldı.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 from .forms import *
 from .filters import *
@@ -15,6 +17,9 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
+            return redirect('accounts:login')
 
     context = {
         'form' : form
@@ -23,8 +28,20 @@ def registerPage(request):
 
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('accounts:home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
     context = {}
     return render(request, 'accounts/login.html', context)
+
+
+
 
 
 # anasayfa
