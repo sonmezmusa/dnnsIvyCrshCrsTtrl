ac739a4267a3a94d51db507018e93ab43004f26b
sonmezmusa <sonmezmusa@outlook.com>
2020-12-31 17:06:27 +0300

    accounts/decorators.py değiştirildi
    Kullanıcının 'group'una göre return yaptıran 'admin_only'
    decorator'ü tanımlandı.

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -24,3 +24,17 @@ def allowed_users(allowed_roles=[]):
                 return HttpResponse('<h1>You are not authorized to view this page</h1>')
         return wrapper_func
     return decorator
+
+
+def admin_only(view_func):
+    def wrapper_function(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+
+        if group == 'customer':
+            return redirect('accounts:user_page')
+
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_function
