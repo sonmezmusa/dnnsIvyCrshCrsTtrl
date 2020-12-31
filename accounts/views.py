3b99db65c07a401984d375585dab46b8ea2719f5
sonmezmusa <sonmezmusa@outlook.com>
2020-12-31 22:14:02 +0300

    accounts/views.py değiştirildi
    Kullanıcı bilgilerini form ile değiştirebilmek için
    'accountSettings' view eklendi.

--- accounts/views.py
+++ accounts/views.py
@@ -100,6 +100,23 @@ def userPage(request):
     return render(request, 'accounts/user.html', context)
 
 
+@login_required(login_url='accounts:login')
+@allowed_users(allowed_roles=['admin', 'customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/account_settings.html', context)
+
+
 # ürünler
 @login_required(login_url='accounts:login')
 @allowed_users(allowed_roles=['admin'])
