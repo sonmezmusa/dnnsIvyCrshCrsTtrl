db036688499f7c0b0c1c6e9b533955fc804bdd29
sonmezmusa <sonmezmusa@outlook.com>
2020-12-31 17:50:04 +0300

    accounts/views.py değiştirildi
    'registerPage' view'de oluşturulan 'user', 'Customer'
    modelindeki ilişkilendirilmiş 'user' değerine atandı.

--- accounts/views.py
+++ accounts/views.py
@@ -21,8 +21,14 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
+            
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+
+            Customer.objects.create(
+                user = user
+            )
+            
             messages.success(request, 'Account was created for ' + username)
             return redirect('accounts:login')
     context = {
@@ -76,8 +82,21 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
+@login_required(login_url='accounts:login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    #print('ORDERS: ', orders)
+    context = {
+        'orders' : orders,
+        'total_orders' : total_orders,
+        'delivered' : delivered,
+        'pending' : pending
+    }
     return render(request, 'accounts/user.html', context)
 
 
