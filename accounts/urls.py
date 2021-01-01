e6d3229cc9205768c988d93489e3ebd03acabcb7
sonmezmusa <sonmezmusa@outlook.com>
2021-01-01 16:52:04 +0300

    accounts/urls.py değiştirildi
    sending email işlemleri için path'ler eklendi.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 app_name = 'accounts'
@@ -19,4 +20,19 @@ urlpatterns = [
     path('create_order/<str:pk>/', createOrder, name="create_order"),
     path('update_order/<str:pk>/', updateOrder, name='update_order'),
     path('delete_order/<str:pk>/', deleteOrder, name='delete_order'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
+
+
+
+# All authentication views
+# https://docs.djangoproject.com/en/3.1/topics/auth/default/#all-authentication-views
+
+# 1 - Submit email form                         //PasswordResetView.as_view()
+# 2 - Email sent success message                //PasswordResetDoneView.as_view()
+# 3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+# 4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
